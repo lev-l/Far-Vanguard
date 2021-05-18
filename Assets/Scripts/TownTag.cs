4dcb84f6913f3e0a3daebfb52f714594349c1798
Leo <balashov.leo@gmail.com>
2021-05-18 20:04:39 +0300

    Correct adding AI`s castle to Towns list
    Now the AI`s castle position in Towns list is correct. Before it was (0, 0), becouse TownTag sign this position too fast, when map steel load.

--- Assets/Scripts/TownTag.cs
+++ Assets/Scripts/TownTag.cs
@@ -7,6 +7,11 @@ public class TownTag : MonoBehaviour
     {
         gameObject.AddComponent<SquadsRoom>();
         gameObject.AddComponent<TownFight>();
+        Invoke(nameof(AddToTownsList), 0.01f);
+    }
+
+    public void AddToTownsList()
+    {
         TownsContainer.Towns
             .Add(Grid.VectorToGridPosition(transform.position),
                                                             this);
